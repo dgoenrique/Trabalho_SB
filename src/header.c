95dbafffac7bc2d57663c7573eb5d24eb6011c45
Marcos Reges Mota <107368603+Regittos@users.noreply.github.com>
2023-01-13 11:40:09 -0300

    Update

--- src/header.c
+++ src/header.c
@@ -1,7 +1,11 @@
 #include "header.h"
+
+// TODO
+// * ATRIBUIÇÃO
+
 int qi = 0;        // Quantidade de ifs
 short int pos[10]; // posição dos elementos em uma pilha do vetor
-char posp = 0;     // quantidade de parâmetros de uma função
+char posparam = 0; // quantidade de parâmetros de uma função
 void defFunct()
 {
   sscanf(str, "%s %s %s %s %s", s1, s2, s3, s4, s5);
@@ -11,11 +15,11 @@ void defFunct()
   printf("movq %s, %s\n", registradores64[7], registradores64[6]);
 
   if (!strcmp(s3, "pa1") || !strcmp(s3, "pi1")) // verifica se há parâmetros
-    posp++;
+    posparam++;
   if (!strcmp(s4, "pa2") || !strcmp(s4, "pi2"))
-    posp++;
+    posparam++;
   if (!strcmp(s5, "pa3") || !strcmp(s5, "pi3"))
-    posp++;
+    posparam++;
 
   printf("\n");
   printf("Definição da função\n");
@@ -36,33 +40,33 @@ void defLocalVar()
       sscanf(str, "%s %s", s1, s2);
       cont += 4;
       pos[ax - 1] = cont;
-      printf("# %s: -%d \n", s2, cont, registradores64[6]);
+      printf("   # %s: -%d \n", s2, cont, registradores64[6]);
     }
     else if (!strcmp("vet", s1)) // Se for um vetor
     {
       sscanf(str, "%s %2s %d %s %2s %d", s1, s2, &ax, s3, s4, &a1);
       cont += 4 * a1;
       pos[ax - 1] = cont;
-      printf("# %s: -%d \n", s2, cont, registradores64[6]);
+      printf("  # %s%d: -%d \n", s2, ax, cont, registradores64[6]);
     }
     else if (!strcmp("enddef", s1)) // salva os parametros na pilha
     {
-      if (posp != 0)
+      if (posparam != 0)
       {
         int aux = cont;
-        aux = aux + posp * 8;
+        aux = aux + posparam * 8;
         if (aux % 16)
 
           aux = aux + 16 - (aux % 16);
         printf("    subq $%d, %s\n\n", aux, registradores64[7]);
         if (cont % 8)
           cont = cont + 8 - (cont % 8);
-        aux = posp;
-        while (posp--)
+        aux = posparam;
+        while (posparam--)
         {
           cont += 8;
           pos[7 + (3 - aux)] = cont; // Salva o endereço dos parâmetros
-          printf("    movq %s, -%d(%s)\n", registradores64[3 + (aux)], cont, registradores64[6]);
+          printf("    movq %s, -%d(%s)\n", registradores64[2 + (aux)], cont, registradores64[6]);
           aux--;
         }
       }
@@ -145,7 +149,6 @@ void funcExpressoes()
   }
   else // EXPRESSÃO SIMPLES
   {
-
     sscanf(s1, "%2s %d", s2, &a1);
     sscanf(s3, "%2s %d", s7, &ax);
     if (!strcmp("vi", s7))
@@ -167,56 +170,60 @@ void atribuicao()
 {
   sscanf(str, "%s %s %s", s1, s2, s3);
 
-  if (!strcmp("call", s3)) // VERIFICA SE O QUE ESTÁ SENDO CHAMADO É UMA FUNÇÃo
+  if (!strcmp("call", s3)) 
     funcCall();
-  else // CASO NÃO SEJA CHAMADA DE FUNÇÃO, AQUI É DESTINADO À EXPRESSÕES
+  else 
     funcExpressoes();
   printf("\n");
 }
 
 void accessArrayGet()
 {
-  // regisamare 2,3,4  -> usar os registradores r7, r8, r9 para o acesso dos
-  // vetores
   printf("Definição do get das variáveis de um vetor\n"); // aqui
-  sscanf(str, "%s %s %s %s %s %s", s1, s2, s3, s4, s5, s6);
-  printf("    movq $%s, %s\n", s2, registradores64[8]);
+  sscanf(str, "%s %2s %d %s %2s %d %s %2s %d", s1, s2, &ax, s3, s4, &a1, s5, s6, &a2);
 
-  sscanf(s4, "%2s %d\n", s7, &ax);
-  printf("    movq $%d, %s\n", ax, registradores64[9]);
-  printf("    imulq $4, %s\n", registradores64[9]);
-  printf("    addq %s, %s\n", registradores64[8], registradores64[9]); // até aqui é o mesmo do bloco de cima
+  printf("    movq $%d, %s\n", a1, registradores64[8]);
 
-  printf("    movl (%s), %s\n", registradores64[9], registradores32[10]);
-  printf("    movl %s, %s\n", registradores32[10], s6); // recuperação de uma variável
-                                                        // printf("%s\n", s6);
+  printf("    imulq $4, %s\n", registradores64[8]);
+
+  if (!strcmp(s2, "va"))
+    printf("    leaq -%d(%s), %s\n", pos[ax - 1], registradores64[6], registradores64[9]);
+  else
+    printf("    leaq -%d(%s), %s\n", pos[6 + ax], registradores64[6], registradores64[9]);
+
+  printf("    addq %s, %s\n", registradores64[9], registradores64[8]);
+
+  printf("    movl (%s), %s\n", registradores64[8], registradores32[9]);
+
+  printf("    movl %s, -%d(%s)\n", registradores32[9], pos[a2 - 1], registradores64[6]);
+
   printf("\n");
 }
 
-// Delarar variáveis na pilha
-
 void accessArraySet()
 {
-  // vetores
-
   printf("Definição do set das variáveis de um vetor\n");
-  sscanf(str, "%s %s %s %s %s %s", s1, s2, s3, s4, s5, s6);
-  printf("    movq $%s, %s\n", s2, registradores64[8]);
-  sscanf(s4, "%2s %d", s7, &ax);
-  printf("    movq $%d, %s\n", ax, registradores64[9]);
-  printf("    imulq $4, %s\n", registradores64[9]);
-  printf("    addq %s, %s\n", registradores64[8], registradores64[9]);
-  sscanf(s6, "%2s %d\n", s7, &ax);
-  // atribuição de constante
-  if (!strcmp("ci", s7))
-    // printf("%d\n", ax);
-    printf("    movq $%d, (%s)\n", ax, registradores64[9]);
-  else // atribuição através de variável
+  sscanf(str, "%s %2s %d %s %2s %d %s %2s %d", s1, s2, &ax, s3, s4, &a1, s5, s6, &a2);
+
+  printf("    movq $%d, %s\n", a1, registradores64[8]);
+
+  printf("    imulq $4, %s\n", registradores64[8]);
+
+  if (!strcmp(s2, "va"))
+    printf("    leaq -%d(%s), %s\n", pos[ax - 1], registradores64[6], registradores64[9]);
+  else
+    printf("    leaq -%d(%s), %s\n", pos[6 + ax], registradores64[6], registradores64[9]);
+
+  printf("    addq %s, %s\n", registradores64[9], registradores64[8]);
+  if (!strcmp("ci", s6))
+    printf("    movl $%d, (%s)", a2, registradores64[8]);
+  else
   {
-    printf("    movl %s, %s\n", s6, registradores32[10]);
-    printf("    movslq %s, (%s)\n", registradores32[10], registradores64[9]);
+    printf("    movl -%d(%s), %s\n", pos[a2 - 1], registradores64[6], registradores32[9]);
+
+    printf("    movl %s, (%s)\n", registradores32[9], registradores64[8]);
   }
-  // printf("%s\n", s6);
+  printf("\n");
 }
 
 void condicionalIf()
@@ -233,7 +240,6 @@ void condicionalEndif()
 
   if (!strcmp("endif", s1))
     printf("end_if%d:\n", qi);
-  // ACESSAR ARRAYS E CRIA CONDICIONAIS
 }
 
 void retorno()
@@ -249,4 +255,8 @@ void retorno()
     printf("    movl $%d, %s\n", ax, registradores32[0]);
   printf("\n");
 }
-void leave() { printf("leave\nret\n\n"); }
+void leave()
+{
+  memset(pos, 0, sizeof(pos));
+  printf("leave\nret\n\n");
+}
