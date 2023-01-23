f31d765558ecdb38684f063650d4a0cf43107bf3
Marcos Reges Mota <107368603+Regittos@users.noreply.github.com>
2023-01-23 20:02:32 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,15 +1,15 @@
 #include "header.c"
 
 // TODO
-// * ATRIBUIÇÃO
+// * divisão
 
 int main() // Código em C
 {
   printf(".section .rodata\n");
   printf(".data\n");
   printf(".text\n\n");
-
-  while (1) {
+  while (1)
+  {
     // Limpeza dos lixos dentro das variaveis
     memset(str, 0, sizeof(str));
     memset(s1, 0, sizeof(s1));
@@ -19,37 +19,50 @@ int main() // Código em C
     memset(s5, 0, sizeof(s5));
     memset(s6, 0, sizeof(s6));
     memset(s7, 0, sizeof(s7));
-    scanf("%[^\n]%*c", str);
+
+    if (scanf("%[^\n]%*c", str) == EOF)
+      break;
+    if (!strlen(str))
+      scanf("%*c");
 
     // printf("%s\n", sr);
     //  sscanf(str, "%s %s %s %s %s %s", s1, s2, s3, s4, s5, s6);
     sscanf(str, "%s", s1);
     //
-    if (!strcmp("function", s1)) {
+    if (!strcmp("function", s1))
+    {
       defFunct();
-    } else if (!strcmp("def", s1)) {
+    }
+    else if (!strcmp("def", s1))
+    {
       defLocalVar();
-    } else if (!strcmp("return", s1)) // AQUI É DEFINIDA O RETORNO DA FUNÇÃO
+    }
+    else if (!strcmp("return", s1)) // AQUI É DEFINIDA O RETORNO DA FUNÇÃO
       retorno();
     else if (!strcmp("end", s1)) // AQUI SE LOCALIZA O FIM DA FUNÇÃO
     {
-      leave();
-      return 0;
-    } else if ('v' == s1[0] && 'i' == s1[1]) // SEÇÃO DE ATRIBUIÇÃO DE VARIÁVEIS
+      memset(pos, 0, sizeof(pos));
+      posparam = 0;
+    }
+    else if ('v' == s1[0] && 'i' == s1[1]) // SEÇÃO DE ATRIBUIÇÃO DE VARIÁVEIS
     {
       atribuicao();
-    } else if (!strcmp("if", s1)) // Definição do IF
+    }
+    else if (!strcmp("if", s1)) // Definição do IF
     {
       condicionalIf();
-    } else if (!strcmp("endif", s1))
+    }
+    else if (!strcmp("endif", s1))
       condicionalEndif();
     // ACESSAR ARRAYS E CRIAR CONDICIONAIS
 
-    else if (!strcmp("set", s1)) {
+    else if (!strcmp("set", s1))
+    {
       accessArraySet();
     }
 
-    else if (!strcmp("get", s1)) {
+    else if (!strcmp("get", s1))
+    {
       accessArrayGet();
     }
     printf("\n");
